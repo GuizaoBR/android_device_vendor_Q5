5af012279cce10b1174573dde45f209c754347ef
GuizaoBR <gui.germano.silva@gmail.com>
2017-05-29 09:01:46 -0300

    Sources based from k4 note

---
+++ Q5-vendor-blobs.mk
@@ -0,0 +1,15 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Quantum/Q5/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Quantum/Q5/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Quantum/Q5/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Quantum/Q5/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Quantum/Q5/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Quantum/Q5/thh,system/thh)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Quantum/Q5/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Quantum/Q5/vendor,system/vendor)
